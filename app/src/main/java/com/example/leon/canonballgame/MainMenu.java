a6b45de6c3d1b7f10502e89233462f0fc8846748
codelikeleon <leonschiedermair@hotmail.co.uk>
2017-03-16 20:15:21 +0000

    Menu buttons highlight green when selected

--- app/src/main/java/com/example/leon/canonballgame/MainMenu.java
+++ app/src/main/java/com/example/leon/canonballgame/MainMenu.java
@@ -1,125 +1,84 @@
 package com.example.leon.canonballgame;
 
-import android.annotation.SuppressLint;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ToggleButton;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class MainMenu extends AppCompatActivity {
+public class MainMenu extends AppCompatActivity implements Button.OnClickListener {
 
-
-    private ToggleButton btn_level_1;
-    private ToggleButton btn_level_2;
-    private ToggleButton btn_level_3;
-    private ToggleButton btn_level_4;
-    private ToggleButton btn_level_5;
+    private Button btn_lvl_1;
+    private Button btn_lvl_2;
+    private Button btn_lvl_3;
+    private Button btn_lvl_4;
+    private Button btn_lvl_5;
+    private Button btn_play;
 
     private int levelSelected;
 
     public void setLevel( int level ) {
         levelSelected = level;
     }
-//    /**
-//     * Whether or not the system UI should be auto-hidden after
-//     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-//     */
-//    private static final boolean AUTO_HIDE = true;
-//
-//    /**
-//     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-//     * user interaction before hiding the system UI.
-//     */
-//    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-//
-//    /**
-//     * Some older devices needs a small delay between UI widget updates
-//     * and a change of the status and navigation bar.
-//     */
-//    private static final int UI_ANIMATION_DELAY = 300;
-//    private final Handler mHideHandler = new Handler();
-//    private View mContentView;
-//    private final Runnable mHidePart2Runnable = new Runnable() {
-//        @SuppressLint("InlinedApi")
-//        @Override
-//        public void run() {
-//            // Delayed removal of status and navigation bar
-//
-//            // Note that some of these constants are new as of API 16 (Jelly Bean)
-//            // and API 19 (KitKat). It is safe to use them, as they are inlined
-//            // at compile-time and do nothing on earlier devices.
-//            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-//                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-//                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-//        }
-//    };
-//    private View mControlsView;
-//    private final Runnable mShowPart2Runnable = new Runnable() {
-//        @Override
-//        public void run() {
-//            // Delayed display of UI elements
-//            ActionBar actionBar = getSupportActionBar();
-//            if (actionBar != null) {
-//                actionBar.show();
-//            }
-//            mControlsView.setVisibility(View.VISIBLE);
-//        }
-//    };
-//    private boolean mVisible;
-//    private final Runnable mHideRunnable = new Runnable() {
-//        @Override
-//        public void run() {
-//            //hide();
-//        }
-//    };
-//    /**
-//     * Touch listener to use for in-layout UI controls to delay hiding the
-//     * system UI. This is to prevent the jarring behavior of controls going away
-//     * while interacting with activity UI.
-//     */
-//    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-//        @Override
-//        public boolean onTouch(View view, MotionEvent motionEvent) {
-//            if (AUTO_HIDE) {
-//                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-//            }
-//            return false;
-//        }
-//    };
+
+    public void setSelectedLevelButtonColour( Button btn ) {
+
+        btn_lvl_1.setBackgroundColor(0xFF3079AB);
+        btn_lvl_2.setBackgroundColor(0xFF3079AB);
+        btn_lvl_3.setBackgroundColor(0xFF3079AB);
+        btn_lvl_4.setBackgroundColor(0xFF3079AB);
+        btn_lvl_5.setBackgroundColor(0xFF3079AB);
+
+        btn.setBackgroundColor(0xFF33CC33);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main_menu);
 
-//        mVisible = true;
-//        //mControlsView = findViewById(R.id.fullscreen_content_controls);
-//        mContentView = findViewById(R.id.fullscreen_content);
-//
-//
-//        // Set up the user interaction to manually show or hide the system UI.
-//        mContentView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                toggle();
-//            }
-//        });
+        btn_lvl_1 = (Button) findViewById(R.id.menu_lvl_1_btn); btn_lvl_1.setOnClickListener(this);
+        btn_lvl_2 = (Button) findViewById(R.id.menu_lvl_2_btn); btn_lvl_2.setOnClickListener(this);
+        btn_lvl_3 = (Button) findViewById(R.id.menu_lvl_3_btn); btn_lvl_3.setOnClickListener(this);
+        btn_lvl_4 = (Button) findViewById(R.id.menu_lvl_4_btn); btn_lvl_4.setOnClickListener(this);
+        btn_lvl_5 = (Button) findViewById(R.id.menu_lvl_5_btn); btn_lvl_5.setOnClickListener(this);
+    }
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.menu_lvl_1_btn:
+                levelSelected = 1;
+                setSelectedLevelButtonColour(btn_lvl_1);
+                break;
+
+            case R.id.menu_lvl_2_btn:
+                levelSelected = 2;
+                setSelectedLevelButtonColour(btn_lvl_2);
+                break;
+
+            case R.id.menu_lvl_3_btn:
+                levelSelected = 3;
+                setSelectedLevelButtonColour(btn_lvl_3);
+                break;
+
+            case R.id.menu_lvl_4_btn:
+                levelSelected = 4;
+                setSelectedLevelButtonColour(btn_lvl_4);
+                break;
+
+            case R.id.menu_lvl_5_btn:
+                levelSelected = 5;
+                setSelectedLevelButtonColour(btn_lvl_5);
+                break;
+
+        }
     }
 
 //    @Override
